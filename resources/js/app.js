827ce2e56a25ee5108e7654bf9265c0312014c06
Patrício Alex <46056058+patricioalex96@users.noreply.github.com>
2020-10-16 11:01:24 -0300

    Implementando Vue e VueRouter

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
 require('./bootstrap');
+
+
+
+/**
+ * Importando o VueJs
+ */
+import Vue from 'vue'
+
+/**
+ * Importando o router
+ */
+import router from './router/router'
+
+/**
+ * Componentes global
+ */
+
+
+ /**
+  * Id global do VueJs
+  */
+ const app = new Vue({
+     el: "#app",
+     router
+ })
---
+++ resources/js/components/home.vue
@@ -0,0 +1,13 @@
+<template>
+<div>afdasdf</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
