1e350d38fb6d7f271f37b185a31e9aed81ffd1b3
Patrício Alex <46056058+patricioalex@users.noreply.github.com>
2020-11-06 23:50:01 -0300

    Componentes Load e loading
    Loading - Um tela de carregamento para um página toda usado no promise para aparecer enquanto API é consumida.
    
    Load - Um tela de carregamento para carregamento local  usado na promise para aparecer enquanto API é consumida.
    
    Elas são ativadas quando um evento acotecendo enviadando um valor true.

---
+++ resources/js/components/load/Loading.vue
@@ -0,0 +1,36 @@
+<template>
+<div v-if="showLoad">
+    <div class="text-center">
+        <div class="spinner-border" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        Carregando
+    </div>
+    <div class="modal-backdrop fade show"></div>
+</div>
+</template>
+
+<script>
+export default {
+    created() {
+        this.$root.$on('Loading', (active) => {
+            this.showLoad = active
+        })
+    },
+
+    data() {
+        return {
+            showLoad: false
+        }
+    }
+}
+</script>
+
+<style>
+.loading {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    z-index: 99999;
+}
+</style>
---
+++ resources/js/components/load/load.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="container" v-if="showLoad">
+    <div class="load text-center">
+        <div class="spinner-border" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        Carregando
+    </div>
+
+</div>
+</template>
+
+<script>
+export default {
+    created() {
+        this.$root.$on('load', (active) => {
+            this.showLoad = active
+        })
+    },
+
+    data() {
+        return {
+            showLoad: false
+        }
+    }
+}
+</script>
+
+<style>
+.load {
+    position: relative;
+    padding-top: 50px;
+    padding-bottom: 50px;
+    z-index: 1040;
+}
+</style>
