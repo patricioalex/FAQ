6ac4d8692b358609992e6a2cea46e1ec0faa805b
Patrício Alex <46056058+patricioalex@users.noreply.github.com>
2020-11-06 23:24:02 -0300

    Adicionado o realacionamento  um para um.
    Um método foi adiconado dentro de cada tabela( PerguntaResposta, CodigoErro) para criar o relacionamento um para um das tabelas frequencia pergunta e codigo para ser usado na atualização que sempre adiciona +1 quando for lido a pergunta e codigo.

--- app/Models/CodigoErro.php
+++ app/Models/CodigoErro.php
@@ -2,19 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CodigoErro extends Model
 {
-     /**
-     * Definido a tabela e os campos a serem acessados.
-     *
-     * O importante é a tabela  do shema para o model saber qual acessar.
-     * Porém, se tiver mais schemas no banco de dados, é importante definir o nome do schema e a tabela separando por ponto.
-     *
-     *
-     */
+    use HasFactory;
+
     protected $table = 'faq.codigos';
 
-    // protected $fillable = ['sistema_id', 'codigo', 'descricao', 'solucao'];
+    /**
+     * Um para um
+     *
+     * @return void
+     */
+    public function frequenciaID(){
+        return $this->hasOne(FrequenciaCodigo::class, 'id_codigo');
+    }
 }
--- app/Models/PerguntaResposta.php
+++ app/Models/PerguntaResposta.php
@@ -2,24 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PerguntaResposta extends Model
 {
-    /**
-     * Definido a tabela e os campos a serem acessados.
-     *
-     * O importante é a tabela  do shema para o model saber qual acessar.
-     * Porém, se tiver mais schemas no banco de dados, é importante definir o nome do schema e a tabela separando por ponto.
-     *
-     *
-     */
-    
+    use HasFactory;
+
     protected $table = 'faq.perguntas';
-    // protected $fillable = ['sistema_id', 'pergunta', 'resposta',];
 
-    // public function frequenciaId(){
-    //     return $this->hasOne(Frequencia::class, 'id_pergunta');
-    // }
-
+    /**
+     * Um para um
+     *
+     * @return void
+     */
+    public function frequenciaID(){
+        return $this->hasOne(FrequenciaPergunta::class, 'id_pergunta');
+    }
 }
