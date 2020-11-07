3d6fbc6981505d6bceaf56cf5eb6a918c0792f36
Patrício Alex <46056058+patricioalex@users.noreply.github.com>
2020-11-06 23:41:15 -0300

    Popper.js e jQuery inseridos

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,11 +8,12 @@ window._ = require('lodash');
 
 window.axios = require('axios');
 
-// try {
-//     window.Popper = require('po')
-// } catch (error) {
-    
-// }
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {}
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
