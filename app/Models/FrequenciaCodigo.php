6ae0654deceab809b90a4d763dd40752bfe61cac
Patrício Alex <46056058+patricioalex@users.noreply.github.com>
2020-11-06 23:34:08 -0300

    Tabela Frequencia removida e criado novas.
    Frequencia - removido
    
    FrequenciaPergunta - criado
    
    FrequenciaCodigo - Criado
    
    Essas tabelas são responsáveis pela frequencia das perguntas e codigos lidos ou pesquisados no FAQ que posteriomente serão listados ao entrar no site.

---
+++ app/Models/FrequenciaCodigo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FrequenciaCodigo extends Model
+{
+    use HasFactory;
+    
+    protected $table = 'faq.frequencia_codigos';
+}
